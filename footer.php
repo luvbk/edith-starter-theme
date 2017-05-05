6efa9a0d2054d6e60e93b23ec29ecb3ed35347d7
Laurent Verbeke <laurent.verbeke@gmail.com>
2017-05-05 17:36:15 +0200

    Posts navigation styling

--- footer.php
+++ footer.php
@@ -15,11 +15,13 @@
 	</div><!-- #container -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'edith-starter-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'edith-starter-theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'edith-starter-theme' ), 'edith-starter-theme', '<a href="https://automattic.com/" rel="designer">Laurent Verbeke</a>' ); ?>
-		</div><!-- .site-info -->
+		<div class="container">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'edith-starter-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'edith-starter-theme' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'edith-starter-theme' ), 'edith-starter-theme', '<a href="https://automattic.com/" rel="designer">Laurent Verbeke</a>' ); ?>
+			</div><!-- .site-info -->
+		</div><!-- #container -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
